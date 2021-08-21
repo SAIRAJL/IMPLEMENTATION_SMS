0341054f1318fb15812e1f08b4180a0f78113bcf
JuhilMohadikar <86512966+JuhilMohadikar@users.noreply.github.com>
2021-08-21 16:24:09 +0530

    Update list.h

--- inc/list.h
+++ inc/list.h
@@ -1,1 +1,14 @@
+#ifndef __LIST_H
+#define __LIST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include<string.h>
+#include<dos.h>
+#include <math.h>
+
+void list(void);
+
+
+#endif
