10cc7dc88086a2bb527b4556966c9c6d3f56ff7e
shrishti-1309 <69230664+shrishti-1309@users.noreply.github.com>
2021-08-21 21:14:01 +0530

    Create add.h

---
+++ inc/add.h
@@ -0,0 +1,10 @@
+#ifndef __ADD_STUDENT_H__
+#define __ADD_STUDENT_H__
+
+#include "Student.h"
+
+int Add_Student(Student_t *Student);
+
+void create_new_Student(Student_t *Student);
+
+#endif  //__ADD_STUDENT_H__
