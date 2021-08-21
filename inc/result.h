6d0e92b3230d3bed97a5eb762483473057e5b29d
Divas252 <86414591+Divas252@users.noreply.github.com>
2021-08-21 16:13:45 +0530

    Create result.h

---
+++ inc/result.h
@@ -0,0 +1,14 @@
+#ifndef __RESULT_H
+#define __RESULT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include<string.h>
+#include<dos.h>
+#include<stdlib.h>
+#include<alloc.h>
+#include <math.h>
+
+void result();
+#endif
