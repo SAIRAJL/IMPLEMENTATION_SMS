d555623435ceafd056715b9c56e69343c2411aee
shrishti-1309 <69230664+shrishti-1309@users.noreply.github.com>
2021-08-21 21:16:04 +0530

    Update add.c

--- src/add.c
+++ src/add.c
@@ -1,51 +1,44 @@
-#include "header.h"
+/**
+ * @file Add_Student.c
+ * @author Shrishti Singh
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
-void add()
-{
-    FILE *fp;
-    struct student std;
-    char another ='y';
-    system("cls");
+#include <stdio.h>
+//#include <stdio_ext.h>
+#include <stdlib.h>
 
-    fp = fopen("record.txt","ab+");
-    if(fp == NULL){
-        gotoxy(10,5);
-        printf("Error opening file");
-        exit(1);
-    }
-    fflush(stdin);
-    while(another == 'y')
-    {
-        gotoxy(10,3);
-        printf("<--:ADD RECORD:-->");
-        gotoxy(10,5);
-        printf("Enter details of student.");
-        gotoxy(10,7);
-        printf("Enter Name : ");
-        gets(std.name);
-        gotoxy(10,8);
-        printf("Enter Mobile Number : ");
-        gets(std.mobile);
-        gotoxy(10,9);
-        printf("Enter Roll No : ");
-        scanf("%d",&std.rollno);
-        fflush(stdin);
-        gotoxy(10,10);
-        printf("Enter Course : ");
-        gets(std.course);
-        gotoxy(10,11);
-        printf("Enter Branch : ");
-        gets(std.branch);
-        fwrite(&std,sizeof(std),1,fp);
-        gotoxy(10,15);
-        printf("Want to add of another record? Then press 'y' else 'n'.");
-        fflush(stdin);
-        another = getch();
-        system("cls");
-        fflush(stdin);
-    }
-    fclose(fp);
-    gotoxy(10,18);
-    printf("Press any key to continue.");
-    getch();
-    menu();
+#include "Add_Student.h"
+#include "utility.h"
+
+int Add_Student(Student_t *Student) {
+
+  FILE *fptr;
+  if ((fptr = fopen("Students.bin", "ab+")) == NULL) {
+    return -1;
+  }
+  fwrite(Student, sizeof(Student_t), 1, fptr);
+
+  fflush(stdin);
+  fclose(fptr);
+
+  return 0;
+}
+
+void create_new_Student(Student_t *Student) {
+  printf("\nEnter name: ");
+  get_data(Student->name);
+  printf("\nEnter the Standard: ");
+  get_data(Student->standard);
+  printf("\nEnter Email ID: ");
+  get_data(Student->email_id);
+    printf("\nEnter GPA: ");
+  get_data(Student->GPA);
+    printf("\nEnter Sports: ");
+  get_data(Student->sports);
+}
